0c4ab00cf9f7369109707fdcf31ab39af6acdd8f
Sachin Ahuja <sachinahj@gmail.com>
2018-08-22 21:08:30 -0700

    update timeout

--- ts_aws/ts_aws/sqs.py
+++ ts_aws/ts_aws/sqs.py
@@ -12,8 +12,13 @@ stream_segment_queue = resource.Queue(stream_segment_queue_url)
 clip_queue_url = client.get_queue_url(QueueName=ts_config.get('aws.sqs.clip.name'))['QueueUrl']
 clip_queue = resource.Queue(clip_queue_url)
 
-def send_stream_download(payload):
+def send_stream_segment_download(payload):
     stream_segment_queue.send_message(MessageBody=json.dumps(payload))
 
-def send_stream_clip(payload):
+def send_clip(payload):
     clip_queue.send_message(MessageBody=json.dumps(payload))
+
+def update_timeout_clip(receipt_handle):
+    message = clip_queue.Message(receipt_handle)
+    message.change_visibility(VisibilityTimeout=15)
+
