865e9691c914ea5944c1d29896f5604db4515562
Sachin Ahuja <sachinahj@gmail.com>
2018-08-24 19:10:42 -0700

    add logger

--- ts_file/setup.py
+++ ts_file/setup.py
@@ -7,6 +7,11 @@ setup(
     author='Ahuja',
     license='',
     packages=find_packages(),
-    install_requires=[],
+    install_requires=[
+        "ts_logger==0.1",
+    ],
+    dependency_links=[
+        "git+ssh://git@github.com/sachinahj/ts_shared.git@master#egg=ts_logger-0.1&subdirectory=ts_logger",
+    ],
     zip_safe=False
 )
--- ts_media/setup.py
+++ ts_media/setup.py
@@ -8,7 +8,11 @@ setup(
     license='',
     packages=find_packages(),
     install_requires=[
+        "ts_logger==0.1",
         "ffprobe3==0.1.2",
+    ],
+    dependency_links=[
+        "git+ssh://git@github.com/sachinahj/ts_shared.git@master#egg=ts_logger-0.1&subdirectory=ts_logger",
     ],
     zip_safe=False
 )
