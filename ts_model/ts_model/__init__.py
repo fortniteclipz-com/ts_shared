d2052adfa8377485d37d0d0653518e2460cffc77
Sachin Ahuja <sachinahj@gmail.com>
2018-08-25 19:10:22 -0700

    import all class

---
+++ ts_model/ts_model/__init__.py
@@ -0,0 +1,7 @@
+import os
+
+packages = [os.path.basename(f)[:-3] for f in os.listdir(os.path.dirname(__file__)) if f[-3:] == ".py" and not f.endswith("__init__.py")]
+for p in packages:
+    exec(f"from .{p} import *")
+
+
