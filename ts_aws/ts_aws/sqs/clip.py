8a14cd027262a0da5f71c34ab45d0375d9a5a6fd
Sachin Ahuja <sachinahj@gmail.com>
2018-09-05 20:53:35 -0700

    format

--- ts_aws/ts_aws/sqs/clip.py
+++ ts_aws/ts_aws/sqs/clip.py
@@ -14,12 +14,16 @@ queue = resource.Queue(queue_url)
 
 def send_message(payload):
     logger.info("send_message | start", payload=payload)
-    r = queue.send_message(MessageBody=json.dumps(payload))
+    r = queue.send_message(
+        MessageBody=json.dumps(payload)
+    )
     logger.info("send_message | success", response=r)
 
 def change_visibility(receipt_handle):
     if receipt_handle is not None:
         logger.info("change_visibility | start", receipt_handle=receipt_handle)
         message = queue.Message(receipt_handle)
-        r = message.change_visibility(VisibilityTimeout=15)
+        r = message.change_visibility(
+            VisibilityTimeout=15
+        )
         logger.info("change_visibility | success", response=r)
--- ts_aws/ts_aws/sqs/stream_segment__analyze.py
+++ ts_aws/ts_aws/sqs/stream_segment__analyze.py
@@ -3,6 +3,7 @@ import ts_logger
 
 import boto3
 import json
+import time
 
 logger = ts_logger.get(__name__)
 
@@ -14,12 +15,26 @@ queue = resource.Queue(queue_url)
 
 def send_message(payload):
     logger.info("send_message | start", payload=payload)
-    r = queue.send_message(MessageBody=json.dumps(payload))
+    r = queue.send_message(
+        MessageBody=json.dumps(payload)
+    )
     logger.info("send_message | success", response=r)
+
+def send_messages(payloads):
+    logger.info("send_messages | start", payloads=payloads)
+    r = queue.send_messages(
+        Entries=list(map(lambda p: {
+            'Id': f"{p['stream_id']}-{p['segment']}",
+            'MessageBody': json.dumps(p),
+        }, payloads))
+    )
+    logger.info("send_messages | success", response=r)
 
 def change_visibility(receipt_handle):
     if receipt_handle is not None:
         logger.info("change_visibility | start", receipt_handle=receipt_handle)
         message = queue.Message(receipt_handle)
-        r = message.change_visibility(VisibilityTimeout=15)
+        r = message.change_visibility(
+            VisibilityTimeout=15
+        )
         logger.info("change_visibility | success", response=r)
