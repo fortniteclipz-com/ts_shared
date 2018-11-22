0fb75c292f50d7ee649fd8c80ed298fea26bb578
Sachin Ahuja <sachinahj@gmail.com>
2018-11-22 01:03:26 -0600

    stream initialize and stream segment download

--- ts_aws/ts_aws/rds/stream_segment.py
+++ ts_aws/ts_aws/rds/stream_segment.py
@@ -14,10 +14,10 @@ def save_stream_segment(stream_segment):
     return stream_segment
     logger.info("save_stream_segment | success", stream_segment=stream_segment)
 
-def get_stream_segment(stream_segment_id):
-    logger.info("get_stream_segment | start", stream_segment_id=stream_segment_id)
+def get_stream_segment(stream_id, segment):
+    logger.info("get_stream_segment | start", stream_id=stream_id, segment=segment)
     session = ts_aws.rds.get_session()
-    stream_segment = session.query(ts_model.StreamSegment).filter_by(stream_segment_id = stream_segment_id).first()
+    stream_segment = session.query(ts_model.StreamSegment).filter_by(stream_id=stream_id, segment=segment).first()
     session.close()
     logger.info("get_stream_segment | success", stream_segment=stream_segment)
     if stream_segment is None:
@@ -26,12 +26,16 @@ def get_stream_segment(stream_segment_id):
 
 def save_stream_segments(stream_segments):
     logger.info("save_stream_segments | start", stream_segments_length=len(stream_segments))
+    session = ts_aws.rds.get_session()
+    session.bulk_save_objects(stream_segments)
+    session.commit()
+    session.close()
     logger.info("save_stream_segments | success")
 
 def get_stream_segments(stream_id):
     logger.info("get_stream_segments | start", stream_id=stream_id)
-    logger.info("get_stream_segments | success")
+    logger.info("get_stream_segments | success", stream_segments_length=len(stream_segments))
 
 def get_clip_stream_segments(clip):
     logger.info("get_clip_stream_segments | start", clip=clip)
-    logger.info("get_clip_stream_segments | success")
+    logger.info("get_clip_stream_segments | success", clip_stream_segments_length=len(clip_stream_segments))
--- ts_model/ts_model/StreamMoment.py
+++ ts_model/ts_model/StreamMoment.py
@@ -7,17 +7,13 @@ class StreamMoment(ts_model.Base, ts_model.BaseMixin):
     stream_moment_id = sa.Column('stream_moment_id', sa.Integer, primary_key=True, autoincrement=True)
     stream_id = sa.Column('stream_id', sa.String(255), sa.ForeignKey('streams.stream_id'))
     segment = sa.Column('segment', sa.Integer)
-    moment_id = sa.Column('moment_id', sa.String(255))
     time = sa.Column('time', sa.Float)
     tag = sa.Column('tag', sa.String(255))
-    game = sa.Column('game', sa.String(255))
 
     def __init__(self, **kwargs):
         ts_model.Base.__init__(self, **kwargs)
         self.stream_moment_id = kwargs.get('stream_moment_id')
         self.stream_id = kwargs.get('stream_id')
         self.segment = kwargs.get('segment')
-        self.moment_id = kwargs.get('moment_id')
         self.time = kwargs.get('time')
         self.tag = kwargs.get('tag')
-        self.game = kwargs.get('game')
