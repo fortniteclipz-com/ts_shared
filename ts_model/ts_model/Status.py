4983a1a2c83825578fd60df01bc59e7489aab524
Sachin Ahuja <sachinahj@gmail.com>
2018-11-29 20:00:04 -0800

    READYING

--- ts_model/ts_model/Status.py
+++ ts_model/ts_model/Status.py
@@ -3,7 +3,7 @@ import enum
 class Status(enum.IntEnum):
     ERROR = -1
     NONE = 0
-    INITIALIZING = 1
+    READYING = 1
     READY = 2
     def __get__(self, instance, owner):
         return self.value
