149de2436ba930d3a913e5dccc545e5380ea77de
Sachin Ahuja <sachinahj@gmail.com>
2018-12-01 13:44:23 -0800

    bug fix

--- ts_model/ts_model/Clip.py
+++ ts_model/ts_model/Clip.py
@@ -3,7 +3,7 @@ import ts_model
 import datetime
 import sqlalchemy as sa
 
-class Clip(ts_model.Base, ts_model.BaseMixin):
+class Clip(ts_model.BaseMixin, ts_model.Base):
     __tablename__ = 'clips'
     clip_id = sa.Column('clip_id', sa.String(255), primary_key=True)
     user_id = sa.Column('user_id', sa.String(255))
@@ -13,14 +13,3 @@ class Clip(ts_model.Base, ts_model.BaseMixin):
     media_key = sa.Column('media_key', sa.String(255))
     _status = sa.Column('_status', sa.Integer, default=0)
     _date_created = sa.Column('_date_created', sa.DateTime, default=datetime.datetime.utcnow)
-
-    def __init__(self, **kwargs):
-        ts_model.Base.__init__(self, **kwargs)
-        self.clip_id = kwargs.get('clip_id')
-        self.user_id = kwargs.get('user_id')
-        self.stream_id = kwargs.get('stream_id')
-        self.time_in = kwargs.get('time_in')
-        self.time_out = kwargs.get('time_out')
-        self.media_key = kwargs.get('media_key')
-        self._status = kwargs.get('_status', )
-        self._date_created = kwargs.get('_date_created', )
--- ts_model/ts_model/MontageClip.py
+++ ts_model/ts_model/MontageClip.py
@@ -7,10 +7,3 @@ class MontageClip(ts_model.BaseMixin, ts_model.Base):
     montage_id = sa.Column('montage_id', sa.String(255), primary_key=True)
     clip_id = sa.Column('clip_id', sa.String(255), primary_key=True)
     clip_order = sa.Column('clip_order', sa.Integer)
-
-    def __init__(self, **kwargs):
-        ts_model.Base.__init__(self, **kwargs)
-        self.montage_clip_id = kwargs.get('montage_clip_id')
-        self.montage_id = kwargs.get('montage_id')
-        self.clip_id = kwargs.get('clip_id')
-        self.clip_order = kwargs.get('clip_order')
