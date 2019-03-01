9ddb25d66989620c102c66ac10d0c7ea14bd6b1d
Sachin Ahuja <sachinahj@gmail.com>
2019-02-28 18:47:21 -0800

    TS_ENV

--- ts_config/ts_config/ts_config.py
+++ ts_config/ts_config/ts_config.py
@@ -2,11 +2,11 @@ import os
 import functools
 import yaml
 
-ts_env = os.environ.get('ts_env', 'dev')
-print("ts_config | ts_env |", ts_env)
+TS_ENV = os.environ.get('TS_ENV', 'dev')
+print("ts_config | TS_ENV |", TS_ENV)
 
 dir_path = os.path.dirname(os.path.abspath(__file__))
-yml_path = f"{dir_path}/ts_config_{ts_env}.yml"
+yml_path = f"{dir_path}/ts_config_{TS_ENV}.yml"
 with open(yml_path) as f:
     cfg = yaml.safe_load(f)
 
