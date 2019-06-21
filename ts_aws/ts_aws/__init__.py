06d0497d5e8476a7029caa3c1fcedcb2192a6eae
Sachin Ahuja <sachinahj@gmail.com>
2019-06-21 12:59:19 -0700

    upgrade shared

--- ts_aws/ts_aws/__init__.py
+++ ts_aws/ts_aws/__init__.py
@@ -1,9 +1,10 @@
 import boto3
 import botocore
+import os
 
 import ts_logger
 
 logger = ts_logger.get(__name__)
 
-logger.info("boto3 version", boto3_version=boto3.__version__)
-logger.info("botocore version", botocore_version=botocore.__version__)
+logger.info("ts_aws | boto", boto3_version=boto3.__version__, botocore_version=botocore.__version__)
+logger.info("ts_aws | environment", environ=os.environ, optdir=os.listdir("/opt"))
